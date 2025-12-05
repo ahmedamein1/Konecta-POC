84c322d91e4be42aeefc9c82cb75c1bd19049a72
Ahmed Amin Naem <ahmed.naem@konecta.com>
2025-12-05 13:57:51 +0200

    add todos-data folder with initial todos.json to serve as simple backend storage

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
