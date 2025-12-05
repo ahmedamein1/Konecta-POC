7ad484e8fe40eee55d50400f29e0b0ae0bade486
Ahmed Amin Naem <ahmed.naem@konecta.com>
2025-12-05 14:24:31 +0200

    add todo router in backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const todoRouter = require('./router/todos');
 
 const app = express();
 
 
 app.use(express.json());
+app.use('/todo', todoRouter);
 
 
 const PORT = process.env.PORT || 4000;
