788629dfafb458872af67d89d7e9362f0cfa81f4
Ahmed Amin Naem <ahmed.naem@konecta.com>
2025-12-05 14:29:13 +0200

    implement 404 endpoint handler and global error middleware to standardize API error responses

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const todoRouter = require('./router/todos');
+require("dotenv").config();
+const express = require("express");
+const todoRouter = require("./router/todos");
 
 const app = express();
 
-
 app.use(express.json());
-app.use('/todo', todoRouter);
+app.use("/todo", todoRouter);
 
+
+
+app.use((req, res, next) => {
+  const error = new Error(
+    "The endpoint you're trying to access does not exist."
+  );
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error("GLOBAL ERROR:", err.message);
+
+  res.status(err.status || 500).json({
+    status: err.status || 500,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 
