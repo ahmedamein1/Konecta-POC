7a4b41e82ac2eae174cad55eb7a7dc2839f5bb84
Ahmed Amin Naem <ahmed.naem@konecta.com>
2025-12-06 17:13:42 +0200

    implement global toast notification system using ngx-toastr

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,13 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppLayout } from "./components/app-layout/app-layout";
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
   imports: [AppLayout]
 })
-export class App {
+export class App implements OnInit {
 
+  constructor(private toastr: ToastrService) {}
+
+  ngOnInit() {
+    this.toastr.success('Hello');
+  }
 }
--- frontend-angular/src/main.ts
+++ frontend-angular/src/main.ts
@@ -1,5 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
-bootstrapApplication(App)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-top-right',
+      timeOut: 2500,
+      progressBar: false,
+      closeButton: true,
+      tapToDismiss: true,
+      newestOnTop: true,
+      extendedTimeOut: 2500
+    })
+  ]
+}).catch(err => console.error(err));
