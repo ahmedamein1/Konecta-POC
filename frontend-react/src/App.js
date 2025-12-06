65c0dd232ab6793bbd1544589187a9ba9dcd1f2e
Ahmed Amin Naem <ahmed.naem@konecta.com>
2025-12-06 16:07:45 +0200

    react is done

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import AppLayout from './components/AppLayout/AppLayout';
+import "./App.css";
+import AppLayout from "./components/AppLayout/AppLayout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 
 function App() {
   return (
     <div className="App">
-        <AppLayout />
-        <ToastContainer 
+      <h1 style={{ textAlign: "center" }}>React POC</h1>
+      <AppLayout />
+      <ToastContainer
         position="top-right"
         autoClose={2500}
         pauseOnHover
         hideProgressBar={true}
         theme="light"
       />
-      
     </div>
   );
 }
