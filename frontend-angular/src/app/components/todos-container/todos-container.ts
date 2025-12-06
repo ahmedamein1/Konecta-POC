2c734d5ebc3fa9b8e9b991eb896cdf87ee02a8d3
Ahmed Amin Naem <ahmed.naem@konecta.com>
2025-12-06 20:17:32 +0200

    add status filter dropdown and filteredTodos logic to todos container

--- frontend-angular/src/app/components/todos-container/todos-container.ts
+++ frontend-angular/src/app/components/todos-container/todos-container.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 import { TodosService } from '../../services/todos-service';
@@ -9,17 +10,22 @@ import { SingleTodo } from '../single-todo/single-todo';
 @Component({
   selector: 'app-todos-container',
   standalone: true,
-  imports: [CommonModule, SingleTodo],
+  imports: [CommonModule, FormsModule, SingleTodo],
   templateUrl: './todos-container.html',
   styleUrls: ['./todos-container.css'],
 })
 export class TodosContainer implements OnInit, OnDestroy {
-  todos!: Todo[];
-  fetchLoading!: boolean;
+  
+  todos: Todo[] = [];
+  fetchLoading: boolean = false;
+  filter: string = 'ALL';
 
   private subs = new Subscription();
 
-  constructor(private todosService: TodosService, private cdr: ChangeDetectorRef) {}
+  constructor(
+    private todosService: TodosService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.subs.add(
@@ -29,12 +35,14 @@ export class TodosContainer implements OnInit, OnDestroy {
       })
     );
 
+    
     this.subs.add(
       this.todosService.fetchLoading$.subscribe((isLoading) => {
         this.fetchLoading = isLoading;
         this.cdr.detectChanges();
       })
     );
+
     this.todosService.loadTodos();
   }
 
@@ -42,7 +50,14 @@ export class TodosContainer implements OnInit, OnDestroy {
     this.subs.unsubscribe();
   }
 
-  get isEmpty() {
-    return !this.todos || this.todos.length === 0;
+
+  get filteredTodos(): Todo[] {
+    if (!this.todos) return [];
+    if (this.filter === 'ALL') return this.todos;
+    return this.todos.filter(todo => todo.status === this.filter);
+  }
+
+  get isEmpty(): boolean {
+    return this.filteredTodos.length === 0;
   }
 }
