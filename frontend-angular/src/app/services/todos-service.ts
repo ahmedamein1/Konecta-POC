1620b83685b4dfdd16d5e61c5cdd3e22d48df007
Ahmed Amin Naem <ahmed.naem@konecta.com>
2025-12-06 17:22:27 +0200

    create initial TodosService setup

--- frontend-angular/src/app/services/todos-service.ts
+++ frontend-angular/src/app/services/todos-service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Todo } from '../model/todo.model';
+import { API_URL } from '../config/api-endpoint';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
-  
+
+  private API_URL = API_URL;
+
+  todos$ = new BehaviorSubject<Todo[]>([]);
+  activeTodosCount$ = new BehaviorSubject<number>(0);
+  fetchTodoLoading$ = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) {}
 }
